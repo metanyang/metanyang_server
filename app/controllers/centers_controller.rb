b2dc448e65fcf65d2df24c79187c99d8a9b26011
Jinhyuk Kim <reniowood@gmail.com>
2018-09-01 21:46:07 +0900

    보호소 가져오는 method 추가

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -1,9 +1,14 @@
+require 'net/http'
+require 'nokogiri'
+
 class CentersController < ApplicationController
   before_action :set_center, only: [:show]
   # 업체 목록 api
   def index
     good_id = params[:good_id]
     center_ids = CenterGood.where(good_id: good_id).pluck("center_id")
+
+    get_centers
 
     if good_id.present?
       @centers = Center.where(id: center_ids)
@@ -25,5 +30,33 @@ class CentersController < ApplicationController
   private
   def set_center
     @center = Center.find(params[:id])
+  end
+
+  def get_centers
+    centers = []
+
+    for page in 1..33
+      uri = URI.parse('http://www.animal.go.kr/portal_rnl/map/mapInfo2.jsp')
+      params = { pagecnt: page }
+      uri.query = URI.encode_www_form(params)
+
+      res = Net::HTTP.get_response(uri)
+      data = res.body.bytes.pack('c*').force_encoding('euc-kr').encode('utf-8')
+
+      html = Nokogiri::HTML(data)
+      html.css('tbody').css('tr').each do |tr|
+        columns = tr.css('td')
+
+        if columns.length == 4
+          centers.push({
+            name: columns[1].text,
+            phone: columns[2].text,
+            address: columns[3].text.gsub(/\t|\n/, '').squish
+          })
+        end
+      end
+    end
+
+    centers
   end
 end
