e5d2deee69643f05ddca8f14dcdbc9f9e26fddfd
Jinhyuk Kim <reniowood@gmail.com>
2018-09-02 08:46:58 +0900

    후원물품 이름 추가

--- app/models/sponsership.rb
+++ app/models/sponsership.rb
@@ -6,4 +6,13 @@ class Sponsership < ApplicationRecord
 
   validates :email, presence: true
 
+  def good_name
+    self.good.name
+  end
+
+  def as_json(*)
+    super.tap do |hash|
+      hash[:good_name] = good_name
+    end
+  end
 end
